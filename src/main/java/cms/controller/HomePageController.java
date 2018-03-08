dc538d9c7ceb84d3f1cccec4c9a66c52d3bafa26
Kamil Wiecha <kamil.wiecha.pwr@gmail.com>
2018-03-08 15:50:17 +0100

    HomePageController added

--- src/main/java/cms/controller/HomePageController.java
+++ src/main/java/cms/controller/HomePageController.java
@@ -1,5 +1,11 @@
 package cms.controller;
 
-public class HomePageController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/home")
+public class HomePageController {
+	
+	
 }
